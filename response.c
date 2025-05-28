0f67608bbda6dd10bcd78400728f212ee9b1ad5a
matyas <matyas.noska@icloud.com>
2025-05-28 12:41:00 +0200

    build head

--- response.c
+++ response.c
@@ -10,6 +10,33 @@
 #include <string.h>
 #include <stdio.h>
 
+const char* status_types[] = {
+    "200 OK",
+    "404 Not Found"
+};
+
+const char* connection_types[] = {
+    "close",
+    "keep-alive"
+};
+
+int send_response(const int client_fd, const char* request, const char* root_dir, const char* content, const size_t content_length) {
+    char header[HEADER_SIZE];
+    const char* mime_type = get_mime_type(request, root_dir);
+
+    if (build_head("HTTP", "1.1", "200 OK", mime_type, content_length, "close", header, HEADER_SIZE) != OK) {
+        return ERROR_GENERIC;
+    }
+    if (send_header(client_fd, header, strlen(header), 0) != OK) {
+        return ERROR_GENERIC;
+    }
+    if (send_body(client_fd, content, content_length, 0) != OK) {
+        return ERROR_GENERIC;
+    }
+
+    return OK;
+}
+
 int send_404_response(const int client_fd) {
     const char* body = "<html><body><h1>404 Not Found</h1><a href='index.html'>Go home</a></body></html>";
     char header[HEADER_SIZE];
@@ -20,7 +47,7 @@ int send_404_response(const int client_fd) {
         "Connection: close\r\n"
         "\r\n", strlen(body));
 
-    if (header_length >= sizeof(header)) {
+    if (header_length >= sizeof(header) < 0) {
         return ERROR_OVERFLOW;
     }
 
@@ -34,27 +61,15 @@ int send_404_response(const int client_fd) {
 
     return OK;
 }
-int send_file_response(const int client_fd, const char* request, const char* root_dir, const char* content, const size_t content_length) {
-    char header[HEADER_SIZE];
-    const char* mime_type = get_mime_type(request, root_dir);
 
-    const int header_length = snprintf(header, sizeof(header),
-        "HTTP/1.1 200 OK\r\n"
+int build_head( const char* protocol, const char* version, const char* status, const char* mime_type, const size_t content_length, const char* connection_type, char* header, size_t header_length ) {
+    if (snprintf(header, header_length,
+        "%s/%s %s\r\n"
         "Content-Length: %zu\r\n"
         "Content-Type: %s\r\n"
-        "Connection: close\r\n"
-        "\r\n", content_length, mime_type);
-
-    if (header_length >= sizeof(header)) {
+        "Connection: %s\r\n"
+        "\r\n", protocol, version, status, content_length, mime_type, connection_type) >= (int)header_length) {
         return ERROR_OVERFLOW;
-    }
-
-    if (send_header(client_fd, header, strlen(header), 0) < 0) {
-        return ERROR_GENERIC;
-    }
-
-    if (send_body(client_fd, content, content_length, 0) < 0) {
-        return ERROR_GENERIC;
     }
 
     return OK;
--- response.h
+++ response.h
@@ -8,6 +8,8 @@
 #include <stddef.h>
 
 int send_404_response(int client_fd);
-int send_file_response(int client_fd, const char* request, const char* root_dir, const char* content, size_t content_length);
+int send_response(const int client_fd, const char* request, const char* root_dir, const char* content, const size_t content_length);
+
+int build_head( const char* protocol, const char* version, const char* status, const char* mime_type, const size_t content_length, const char* connection_type, char* header, size_t header_length );
 
 #endif //RESPONSE_H
